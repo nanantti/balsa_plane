ad713616c377df978f1b779cbce64d13659dc41c
Renan Barroso <barrosoren@gmail.com>
2022-02-18 20:22:05 +0200

    Remove string initalizers

--- src/script_engine/polygon/mod.rs
+++ src/script_engine/polygon/mod.rs
@@ -11,81 +11,81 @@ mod tests {
     }
 
     #[test]
-    fn point_init_string() {
-        let point = Point2D::init("0,1");
-        assert_eq!(point.x, 0.0);
-        assert_eq!(point.y, 1.0);
-    }
-
-    #[test]
     fn point_init_string_float() {
-        let point = Point2D::init("-0.3,1.2");
+        let point = Point2D::new(-0.3, 1.2);
         assert_eq!(point.x, -0.3);
         assert_eq!(point.y, 1.2);
     }
 
     #[test]
     fn point_equality() {
-        let p1 = Point2D::init("0.0,1.0");
-        let p2 = Point2D::init("0.0,1.0");
-        let p3 = Point2D::init("1.0,1.0");
+        let p1 = Point2D::new(0.0, 1.0);
+        let p2 = Point2D::new(0.0, 1.0);
+        let p3 = Point2D::new(1.0, 1.0);
         assert!(p1 == p2);
         assert!(p1 != p3);
     }
 
     #[test]
     fn area_simple_vec() {
-        let triang = Polygon::new(vec![Point2D::new(0.0, 0.0), Point2D::new(2.0, 0.0), Point2D::new(1.0, 1.0)]);
+        let triang = Polygon::new(vec![
+            Point2D::new(0.0, 0.0),
+            Point2D::new(2.0, 0.0),
+            Point2D::new(1.0, 1.0),
+        ]);
         assert_eq!(triang.area(), 1.0);
     }
 
     #[test]
     fn area_simple() {
-        let triang = Polygon::init("0,0 2,0 1,1");
+        let triang = Polygon::new(vec![
+            Point2D::new(0.0, 0.0),
+            Point2D::new(2.0, 0.0),
+            Point2D::new(1.0, 1.0),
+        ]);
         assert_eq!(triang.area(), 1.0);
     }
 
     #[test]
     fn centroid_simple() {
-        let triang = Polygon::init("0,0 2,0 1,1");
+        let triang = Polygon::new(vec![
+            Point2D::new(0.0, 0.0),
+            Point2D::new(2.0, 0.0),
+            Point2D::new(1.0, 1.0),
+        ]);
         assert_eq!(triang.centroid().x, 1.0);
         assert_eq!(triang.centroid().y, 1.0 / 3.0);
     }
 
     #[test]
     fn centroid_square() {
-        let triang = Polygon::init("-1,-1 1,-1 1,1 -1,1");
+        let triang = Polygon::new(vec![
+            Point2D::new(-1.0, -1.0),
+            Point2D::new(1.0, -1.0),
+            Point2D::new(1.0, 1.0),
+            Point2D::new(-1.0, 1.0),
+        ]);
         assert_eq!(triang.centroid().x, 0.0);
         assert_eq!(triang.centroid().y, 0.0);
     }
 
     #[test]
-    fn test_read_command() {
-        let triang = Polygon::init("0,0 2,0 1,1");
-        assert_eq!(triang.point_list[0].x, 0.0);
-        assert_eq!(triang.point_list[0].y, 0.0);
-        assert_eq!(triang.point_list[1].x, 2.0);
-        assert_eq!(triang.point_list[1].y, 0.0);
-        assert_eq!(triang.point_list[2].x, 1.0);
-        assert_eq!(triang.point_list[2].y, 1.0);
-    }
-
-    #[test]
-    fn test_read_command_float_negative() {
-        let triang = Polygon::init("0,0.2 2.9,0 -1.1,1");
-        assert_eq!(triang.point_list[0].x, 0.0);
-        assert_eq!(triang.point_list[0].y, 0.2);
-        assert_eq!(triang.point_list[1].x, 2.9);
-        assert_eq!(triang.point_list[1].y, 0.0);
-        assert_eq!(triang.point_list[2].x, -1.1);
-        assert_eq!(triang.point_list[2].y, 1.0);
-    }
-
-    #[test]
     fn polygon_equality() {
-        let p1 = Polygon::init("0,0 2,0 1,1");
-        let p2 = Polygon::init("0,0 2,0 1,1");
-        let p3 = Polygon::init("0,0 2,0 1,2");
+        let p1 = Polygon::new(vec![
+            Point2D::new(0.0, 0.0),
+            Point2D::new(2.0, 0.0),
+            Point2D::new(1.0, 1.0),
+        ]);
+        let p2 = Polygon::new(vec![
+            Point2D::new(0.0, 0.0),
+            Point2D::new(2.0, 0.0),
+            Point2D::new(1.0, 1.0),
+        ]);
+        let p3 = Polygon::new(vec![
+            Point2D::new(0.0, 0.0),
+            Point2D::new(2.0, 0.0),
+            Point2D::new(1.0, 4.0),
+        ]);
         assert!(p1 == p2);
         assert!(p1 != p3);
     }
@@ -93,25 +93,29 @@ mod tests {
     #[test]
     #[should_panic]
     fn zero_points() {
-        let _zerang = Polygon::init("");
+        let _zerang = Polygon::new(vec![]);
     }
 
     #[test]
     #[should_panic]
     fn one_point() {
-        let _oneang = Polygon::init("0,0");
+        let _oneang = Polygon::new(vec![Point2D::new(0.0, 0.0)]);
     }
 
     #[test]
     #[should_panic]
     fn two_points() {
-        let _twoang = Polygon::init("0,0 2,0");
+        let _twoang = Polygon::new(vec![Point2D::new(0.0, 0.0), Point2D::new(0.0, 0.0)]);
     }
 
     #[test]
     #[should_panic]
     fn area_zero() {
-        let _zeroarea = Polygon::init("0,0 1,0 2,0");
+        let _zeroarea = Polygon::new(vec![
+            Point2D::new(0.0, 0.0),
+            Point2D::new(1.0, 0.0),
+            Point2D::new(2.0, 0.0),
+        ]);
     }
 }
 
@@ -122,18 +126,11 @@ pub struct Point2D {
 }
 
 impl Point2D {
-    pub fn init(coordinates: &str) -> Point2D {
-        let coords: Vec<&str> = coordinates.split(',').collect();
-        let x_coord: f32 = coords[0].parse().unwrap();
-        let y_coord: f32 = coords[1].parse().unwrap();
+    pub fn new(x_coord: f32, y_coord: f32) -> Point2D {
         Point2D {
             x: x_coord,
             y: y_coord,
         }
-    }
-
-    pub fn new(x_coord: f32, y_coord: f32) -> Point2D {
-        Point2D { x: x_coord, y: y_coord }
     }
 }
 
@@ -143,16 +140,10 @@ pub struct Polygon {
 }
 
 impl Polygon {
-    pub fn init(coordinates: &str) -> Polygon {
-        let poly = Polygon {
-            point_list: Polygon::get_point_list(coordinates),
-        };
+    pub fn new(points: Vec<Point2D>) -> Polygon {
+        let poly = Polygon { point_list: points };
         poly.sanity_check();
         poly
-    }
-
-    pub fn new(points: Vec<Point2D>) -> Polygon {
-        Polygon { point_list: points }
     }
 
     pub fn area(&self) -> f32 {
@@ -179,14 +170,6 @@ impl Polygon {
         cent.x = cent.x / (count as f32);
         cent.y = cent.y / (count as f32);
         cent
-    }
-
-    fn get_point_list(coordinates: &str) -> Vec<Point2D> {
-        let mut points = Vec::new();
-        for coords in coordinates.split(' ') {
-            points.push(Point2D::init(coords));
-        }
-        points
     }
 
     fn sanity_check(&self) {
